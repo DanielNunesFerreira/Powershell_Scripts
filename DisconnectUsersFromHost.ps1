1f70e8d586c8afd6bdebc9e5e65bc718e348a715
DanielNunesFerreira <40760760+DanielNunesFerreira@users.noreply.github.com>
2019-10-16 14:28:23 +0100

    Create DisconnectUsersFromHost.ps1

---
+++ DisconnectUsersFromHost.ps1
@@ -0,0 +1,52 @@
+# Run this script on Terminal Server
+# If you want to only disconnect who has been idling for 1day/1hour, uncomment below.
+# If you leave it the way it is, it will instantly log-off users that have the state "Disconnected"
+
+$server = "Server1";
+ 
+# Get all RDP sessions
+$sessions = query user /server:$server | select -skip 1;
+ 
+# Loop through each session/line returned
+foreach ($line in $sessions) { 
+ 
+    $line = -split $line;
+ 
+    # Check for missing SessionName field/column
+    if ($line.length -eq 8) {
+ 
+        # Get current session state (column 4)
+        $state = $line[3];
+ 
+        # Get Session ID (column 3) and current idle time (column 5)
+        $sessionid = $line[2]; 
+        $idletime = $line[4];
+ 
+    } else {
+ 
+        # Get current session state (column 3)
+        $state = $line[2];
+ 
+        # Get Session ID (column 2) and current idle time (column 4)
+        $sessionid = $line[1]; 
+        $idletime = $line[3];
+    }
+   
+    # If the session state is Disconnected 
+    if ($state -eq "Disc") { 
+ 
+        #### If you want to only disconnect who has been idling for 1day/1hour, uncomment below. 
+        # Check if idle for more than 1 day (has a '+') and log off 
+        #if ($idletime -like "*+*") {
+ 
+        #    logoff $sessionid /server:$server /v
+ 
+        # Check if idle for more than 1 hour (has a ':') and log off 
+        #} elseif ($idletime -like "*:*") {
+ 
+            logoff $sessionid /server:$server /v
+ 
+        #} 
+    }
+}
+ 
